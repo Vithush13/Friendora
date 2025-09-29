3834d3f4595f7d689e307102c0421a040f2df096
Vithush13 <168434987+Vithush13@users.noreply.github.com>
2025-09-29 22:58:05 +0530

    Remove node_modules from repository

--- Backend/index.js
+++ Backend/index.js
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
