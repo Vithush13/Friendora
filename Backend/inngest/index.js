b65d6a546fff9a2e1d13e2145fab86cb86902514
Vithush13 <168434987+Vithush13@users.noreply.github.com>
2026-02-07 11:09:40 +0530

    Email Connections added

--- Backend/inngest/index.js
+++ Backend/inngest/index.js
@@ -1,5 +1,7 @@
-import { Inngest } from "inngest";
+import { Inngest, step } from "inngest";
 import User from "../model/user.js";
+import { Connection } from "mongoose";
+import sendEmail from "../config/nodeMailer.js";
 
 // Create a Client to send and receive events
 export const inngest = new Inngest({id: "frienora-app"});
@@ -58,8 +60,61 @@ const syncUserDeletion = inngest.createFunction(
 
     }
 )
+
+//Inngest Function to send Remainder when a new connection request is added
+const sendNewConnectionRequestReminder = inngest.createFunction(
+    {id:"send-connection-request-reminder"},
+    {event: "app/connection-request"},
+    async ({event, step}) => {
+        const {connectionId} = event.data;
+
+        await step.run('send-connection-request-mail', async ()=>{
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+            const subject = 'New Connection Request';
+            const body = `<div style="font-family:Arial, sans-serif; padding: 20px;">
+                              <h2>Hi ${connection.to_user_id.full_name}, </h2>
+                              <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+                              <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</P
+                              <br/>
+                              <p>Thanks,<br/>Frindora -Stay Connected</P
+                              </div>`;
+
+                              await sendEmail({
+                                to: connection.to_user_id.email,
+                                subject,body
+                              })
+        })
+
+        const in24Hours = new Date(Date.now() + 24 * 60 *60 * 1000)
+        await step.sleepUntil("wait-for-24-hours", in24Hours);
+        await step.run('send-connection-request-reminder', async ()=>{
+            const connection = await Connection.findById(connectionId).populate('from_user_id to_user_id');
+
+            if(connection.status === "accepted"){
+                return {message: "Already accepted"}
+            }
+
+            const subject = 'New Connection Request';
+            const body = `<div style="font-family:Arial, sans-serif; padding: 20px;">
+                              <h2>Hi ${connection.to_user_id.full_name}, </h2>
+                              <p>You have a new connection request from ${connection.from_user_id.full_name} - @${connection.from_user_id.username}</p>
+                              <p>Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#10b981;">here</a> to accept or reject the request</P
+                              <br/>
+                              <p>Thanks,<br/>Frindora -Stay Connected</P
+                              </div>`;
+
+                              await sendEmail({
+                                to: connection.to_user_id.email,
+                                subject,body
+                              })
+
+                        return {message: "Reminder sent."}      
+        })
+    }
+)
 export const functions = [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
+    sendNewConnectionRequestReminder
 ];
